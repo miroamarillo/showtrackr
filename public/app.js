e5d724aac14d14fb0975e52f61898dbbdb574296
Julian Pineda <studio@miroamarillo.com>
2014-10-01 19:54:49 -0400

    adding route and location providers

--- public/app.js
+++ public/app.js
@@ -4,6 +4,31 @@
 * show tracker app
 */
 angular.module('MyApp', ['ngCookies', 'ngResource', 'ngMessages', 'ngRoute','mgcrea.ngStrap'])
-	.config(function() {
+	.config([ '$locationProvider', '$routeProvider',function($locationProvider, $routeProvider) {
+		$locationProvider.html5Mode(true);
 
-	});
+		$routeProvider
+			.when('/',{
+				templateUrl: 'views/home.html',
+				controller: 'MainController'
+			})
+			.when('/shows/:id',{
+				templateUrl: 'views/detail.html',
+				controller: 'DetailController'
+			})
+			.when('/login',{
+				templateUrl: 'views/login.html',
+				controller: 'LoginController'
+			})
+			.when('/signup',{
+				templateUrl: 'views/signup.html',
+				controller: 'SignupController'
+			})
+			.when('add',{
+				templateUrl: 'views/add.html',
+				controller: 'AddController'
+			})
+			.otherwise({
+				redirectTo: '/'
+			});
+	}]);
