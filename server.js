f05c174c5460ce383a6bc14382dd5b9d57b36747
Julian Pineda <studio@miroamarillo.com>
2014-11-06 21:30:00 -0500

    signup

--- server.js
+++ server.js
@@ -220,7 +220,33 @@ app.post('/api/shows', function(req, res, next) {
   });
 });
 
+app.post('/api/login', passport.authenticate('local'), function(req, res) {
+  res.cookie('user', JSON.stringify(req.user));
+  res.send(req.user);
+});
 
+app.post('/api/signup', function(req, res, next) {
+  var user = new User({
+    email: req.body.email,
+    password: req.body.password
+  });
+  user.save(function(err) {
+    if (err) return next(err);
+    res.send(200);
+  });
+});
+
+app.get('/api/logout', function(req, res, next) {
+  req.logout();
+  res.send(200);
+});
+
+app.use(function(req, res, next) {
+  if (req.user) {
+    res.cookie('user', JSON.stringify(req.user));
+  }
+  next();
+});
 
 app.get('*', function(req, res) {
   res.redirect('/#' + req.originalUrl);
