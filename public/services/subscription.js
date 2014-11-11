1facf86d5695f2bf6bd2135bc90f633138bbea34
Julian Pineda <studio@miroamarillo.com>
2014-11-11 18:49:36 -0500

    updating services

--- public/services/subscription.js
+++ public/services/subscription.js
@@ -1,11 +1,11 @@
 angular.module('MyApp')
-  .factory('Subscription', ['$http', function($http) {
+  .factory('Subscription', function($http) {
     return {
-      subscribe: function(show, user) {
+      subscribe: function(show) {
         return $http.post('/api/subscribe', { showId: show._id });
       },
-      unsubscribe: function(show, user) {
+      unsubscribe: function(show) {
         return $http.post('/api/unsubscribe', { showId: show._id });
       }
     };
-  }]);
+  });
